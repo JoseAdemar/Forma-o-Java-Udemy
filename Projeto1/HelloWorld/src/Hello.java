102961ebd47efc2adf3f5eda8c6024e6dd4dd650
joseademar <joseademarti@gmail.com>
2023-05-16 21:19:28 -0300

    Estudando a  condicional if do java

--- Projeto1/HelloWorld/src/Hello.java
+++
@@ -1,6 +0,0 @@
-public class Hello {
-    public static void main(String[] args) {
-        System.out.println("Hello, Junior");
-        System.out.println("Hello world");
-    }
-}
---
+++ Projeto1/HelloWorld/src/HelloWorld.java
@@ -0,0 +1,16 @@
+public class HelloWorld {
+    public static void main(String[] args) {
+        boolean isAlien = false;
+   //   if (isAlien == false)
+        if (!isAlien) {
+            System.out.println("It is not an alien");
+        }else {
+            System.out.println(" It is an alien");
+        }
+
+        boolean isCar = false;
+        if (isCar = true){
+            System.out.println("This is not supposed to happen");
+        }
+    }
+}
