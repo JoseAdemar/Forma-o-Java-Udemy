9521bf5d2bc5bb3c58689948a64b4e9c9189d757
joseademar <joseademarti@gmail.com>
2023-06-13 21:14:48 -0300

    Estudando estruturas de if e else e if else

--- Projeto1/HelloWorld/src/KeywordsAndExpressios/Main.java
+++ Projeto1/HelloWorld/src/KeywordsAndExpressios/Main.java
@@ -3,5 +3,18 @@ package KeywordsAndExpressios;
 public class Main {
     public static void main(String[] args) {
 
+        boolean gameOver = true;
+        int score = 5000;
+        int levelCompleted = 5;
+        int bonus = 100;
+
+        if(score >= 6000){
+            System.out.println("Seu score é = " + score);
+        }else if(gameOver == false) {
+            System.out.println("sdfsf");
+        }else {
+            System.out.println("nenhuma das opções");
+        }
+
     }
 }
