20a7ce8abb27c85f8b54715d6bab4eb24a272f59
joseademar <joseademarti@gmail.com>
2023-06-20 20:58:14 -0300

    Estudando métodos e chamada de métodos

--- Projeto1/HelloWorld/src/KeywordsAndExpressios/Main.java
+++ Projeto1/HelloWorld/src/KeywordsAndExpressios/Main.java
@@ -2,19 +2,37 @@ package KeywordsAndExpressios;
 
 public class Main {
     public static void main(String[] args) {
-
         boolean gameOver = true;
         int score = 5000;
         int levelCompleted = 5;
         int bonus = 100;
 
-        if(score >= 6000){
-            System.out.println("Seu score é = " + score);
-        }else if(gameOver == false) {
-            System.out.println("sdfsf");
-        }else {
-            System.out.println("nenhuma das opções");
-        }
+        validaMetodos(score,gameOver);
+    }
 
+    public static boolean validaScore(int score) {
+        if (score >= 6000) {
+            System.out.println("Seu score é = " + score);
+            return true;
+        }
+        return false;
+    }
+
+    public static boolean validaGameOver(boolean gameOver) {
+        if (!gameOver) {
+            System.out.println("sdfsf");
+            return true;
+        }
+        return false;
+    }
+
+    public static boolean validaMetodos(int score, boolean gameOver) {
+        var teste1 = validaScore(score);
+        var teste2 = validaGameOver(gameOver);
+        if ((!teste1) && (!teste2)) {
+            System.out.println("nenhuma das opções");
+            return true;
+        }
+        return false;
     }
 }
